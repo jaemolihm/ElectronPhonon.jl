739baeb5fab2f7cd9faa3e0b9e44cec7c24a9bcb
Jae-Mo Lihm <jaemolim96@gmail.com>
2020-12-30 22:47:05 +0900

    More Fourier tests

--- test/test_fourier.jl
+++ test/test_fourier.jl
@@ -25,15 +25,19 @@ using Random
     op_k_normal_2 = Array{ComplexF64,2}(undef, 2, 3)
     op_k_gridopt = Array{ComplexF64,2}(undef, 2, 3)
 
-    get_fourier!(op_k_normal, obj, xk1; mode="normal")
-    get_fourier!(op_k_gridopt, obj, xk1; mode="gridopt")
+    get_fourier!(op_k_normal, obj, xk1, mode="normal")
+    get_fourier!(op_k_gridopt, obj, xk1, mode="gridopt")
     @test op_k_normal ≈ op_k_gridopt
 
-    get_fourier!(op_k_1d, obj, xk1; mode="gridopt")
+    phase = [cis(2π * dot(r, xk1)) for r in irvec]
+    get_fourier!(op_k_normal_2, obj, xk1, phase, mode="normal")
+    @test op_k_normal ≈ op_k_normal_2
+
+    get_fourier!(op_k_1d, obj, xk1, mode="gridopt")
     @test vec(op_k_normal) ≈ op_k_1d
 
-    get_fourier!(op_k_normal, obj, xk2; mode="normal")
-    get_fourier!(op_k_gridopt, obj, xk2; mode="gridopt")
+    get_fourier!(op_k_normal, obj, xk2, mode="normal")
+    get_fourier!(op_k_gridopt, obj, xk2, mode="gridopt")
     @test op_k_normal ≈ op_k_gridopt
 
     # Update op_r
@@ -41,9 +45,9 @@ using Random
     obj_new = WannierObject(nr, irvec, op_r_new)
     update_op_r!(obj, op_r_new)
 
-    get_fourier!(op_k_normal, obj, xk1; mode="normal")
-    get_fourier!(op_k_gridopt, obj, xk1; mode="gridopt")
-    get_fourier!(op_k_normal_2, obj_new, xk1; mode="normal")
+    get_fourier!(op_k_normal, obj, xk1, mode="normal")
+    get_fourier!(op_k_gridopt, obj, xk1, mode="gridopt")
+    get_fourier!(op_k_normal_2, obj_new, xk1, mode="normal")
     @test op_k_normal ≈ op_k_gridopt
     @test op_k_normal ≈ op_k_normal_2
 
