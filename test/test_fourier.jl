ac48ae29865f0222d620d779fae80c01e6689cba
Jae-Mo Lihm <jaemolim96@gmail.com>
2021-01-03 20:54:53 +0900

    Update Fourier test (gridopt case)

--- test/test_fourier.jl
+++ test/test_fourier.jl
@@ -3,13 +3,15 @@ using Test
 using Random
 
 @testset "fourier" begin
-    nr = 3
-    irvec = [Vec3{Int}([0, 0, 0]), Vec3{Int}([1, -2, 1]), Vec3{Int}([1, 3, -2])]
+    irvec = [Vec3{Int}([0, 0, 0]), Vec3{Int}([1, -2, 1]),
+        Vec3{Int}([1, 3, -2]), Vec3{Int}([-1, 3, -2]),
+        Vec3{Int}([-1, -3, 1])]
+    nr = length(irvec)
     Random.seed!(123)
-    op_r = randn(ComplexF64, 6, 3)
+    op_r = randn(ComplexF64, 6, nr)
 
     # Constructor should throw error if irvec is not sorted
-    @test_throws ErrorException obj = WannierObject(4, irvec, op_r)
+    @test_throws ErrorException obj = WannierObject(nr+1, irvec, op_r)
     @test_throws ErrorException obj = WannierObject(nr, irvec, op_r)
 
     ind = sortperm(irvec, by=x->reverse(x))
@@ -28,6 +30,8 @@ using Random
     get_fourier!(op_k_normal, obj, xk1, mode="normal")
     get_fourier!(op_k_gridopt, obj, xk1, mode="gridopt")
     @test op_k_normal ≈ op_k_gridopt
+    @test obj.gridopts[1].nr_23 == 4
+    @test obj.gridopts[1].nr_3 == 3
 
     phase = [cis(2π * sum(r .* xk1)) for r in irvec]
     get_fourier!(op_k_normal_2, obj, xk1, phase, mode="normal")
@@ -41,7 +45,7 @@ using Random
     @test op_k_normal ≈ op_k_gridopt
 
     # Update op_r
-    op_r_new = randn(ComplexF64, 6, 3)
+    op_r_new = randn(ComplexF64, 6, nr)
     obj_new = WannierObject(nr, irvec, op_r_new)
     update_op_r!(obj, op_r_new)
 
