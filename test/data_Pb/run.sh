2d30888f793771740f9554b8f2951e0b4c4fc483
Jae-Mo Lihm <jaemolim96@gmail.com>
2021-09-10 13:56:42 +0900

    Fix test data Pb epw.transport.out

--- test/data_Pb/run.sh
+++ test/data_Pb/run.sh
@@ -3,7 +3,8 @@ set -e
 export OMP_NUM_THREADS=1
 
 # quantum espresso executable
-QE=/home/jmlim/julia_epw/qe-dev/bin
+QE=/home/jmlim/program/qe-dev/bin
+QEJULIA=/home/jmlim/julia_epw/qe-dev/bin
 
 ## SCF, phonon
 #mkdir -p dyn_dir
@@ -13,11 +14,11 @@ QE=/home/jmlim/julia_epw/qe-dev/bin
 #~/bin/epw_pp.py pb
 #rm temp/_ph0/pb.q_*/pb.wfc*
 
-# EPW setup
-mpirun -np 2 $QE/pw.x -nk 2 -ndiag 1 -in scf.in > scf.out
-mpirun -np 2 $QE/pw.x -nk 2 -ndiag 1 -in nscf.in > nscf.out
-mpirun -np 2 $QE/epw.x -nk 2 -in epw_setup.in > epw_setup.out
+## EPW setup
+#mpirun -np 2 $QE/pw.x -nk 2 -ndiag 1 -in scf.in > scf.out
+#mpirun -np 2 $QE/pw.x -nk 2 -ndiag 1 -in nscf.in > nscf.out
+#mpirun -np 2 $QEJULIA/epw.x -nk 2 -in epw_setup.in > epw_setup.out
 
 # EPW
 mpirun -np 1 $QE/epw.x -nk 1 -in epw.transport.in > epw.transport.out
-rm inv_tau* restart.fmt IBTEvel_sup.fmt
+rm inv_tau* restart.fmt IBTEvel_sup.fmt sparse*
