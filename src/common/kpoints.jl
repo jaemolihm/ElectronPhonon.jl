ef7ba76b0d050a673d48988aba0f318eb4835c53
Jae-Mo Lihm <jaemolim96@gmail.com>
2022-02-16 15:32:52 +0900

    Fix small error in kpoints.jl when checking whether k point is on a grid

--- src/common/kpoints.jl
+++ src/common/kpoints.jl
@@ -330,7 +330,7 @@ function unfold_kpoints(kpts::GridKpoints, symmetry)
     for symop in symmetry
         s_shift = symop.is_tr ? -symop.S * shift : symop.S * shift
         dk = s_shift - shift
-        if ! (dk ≈ Vec3(round.(Int, dk.data .* ngrid) ./ ngrid))
+        if norm(dk - Vec3(round.(Int, dk.data .* ngrid) ./ ngrid)) > eps(eltype(dk))
             error("kpts.shift = $(kpts.shift) does not respect the symmetry $symop. Cannot unfold.")
         end
     end
