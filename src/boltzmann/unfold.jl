ef1ce330c8776901d8eb8d91991809a18580d44b
Jae-Mo Lihm <jaemolim96@gmail.com>
2022-02-21 13:41:26 +0900

    Implement unfold_ElectronStates

--- src/boltzmann/unfold.jl
+++ src/boltzmann/unfold.jl
@@ -1,6 +1,65 @@
 export unfold_QMEStates
 
 """
+Unfold `ElectronState` definde on the irreducible BZ `kpts_irr` to the full BZ `kpts`.
+By default, only unfold the energy eigenvalues and eigenvectors. Then, unfold quantities
+listed in `quantities`.
+- `quantities`: Quantities to unfold. Can contain "velocity_diagonal", "velocity", "position".
+"""
+function unfold_ElectronStates(model, states_irr::AbstractVector{ElectronState{FT}}, kpts_irr, kpts, ik_to_ikirr_isym, symmetry; quantities=[], fourier_mode="gridopt") where FT
+    states = ElectronState{FT}[]
+    sym_k = zeros(Complex{FT}, nw, nw)
+    for ik = 1:kpts.n
+        xk = kpts.vectors[ik]
+        ikirr, isym = ik_to_ikirr_isym[ik]
+        xkirr = kpts_irr.vectors[ikirr]
+        symop = symmetry[isym]
+
+        # Energy eigenvalues and ranges are automatically unfolded by copying.
+        push!(states, deepcopy(states_irr[ikirr]))
+
+        # Skip if symmetry is trivial.
+        # FIXME: Change to isone(symop)
+        isym === 1 && continue
+
+        # Unfold eigenvectors.
+        # Find symop in model.el_sym.
+        isym_op = findfirst(s -> s ≈ symop, model.el_sym.symmetry)
+        isym_op === nothing && error("Symmetry $isym not found in model.el_sym.symmetry.")
+        # Compute symmetry operator in Wannier k basis and multiply it to u_full.
+        get_fourier!(sym_k, model.el_sym.operators[isym_op], xkirr; mode=fourier_mode)
+        # Apply SVD to make sym_k unitary.
+        u, s, v = svd(sym_k)
+        mul!(sym_k, u, v')
+        mul!(states[ik].u_full, sym_k, states_irr[ikirr].u_full)
+
+        if "velocity_diagonal" ∈ quantities
+            if symop.is_tr
+                states[ik].vdiag .= .-Ref(symop.Scart) .* states_irr[ikirr].vdiag
+            else
+                states[ik].vdiag .= Ref(symop.Scart) .* states_irr[ikirr].vdiag
+            end
+        end
+
+        if "velocity" ∈ quantities
+            if symop.is_tr
+                states[ik].v .= .-Ref(symop.Scart) .* states_irr[ikirr].v
+            else
+                states[ik].v .= Ref(symop.Scart) .* states_irr[ikirr].v
+            end
+        end
+
+        if "position" ∈ quantities
+            # Symmetry operation of position matrix element involves derivative of the symmetry
+            # matrix. So, we just recalculate it using the new eigenvector.
+            set_position!(states[ik], model, xk, fourier_mode)
+        end
+    end
+    states
+end
+
+
+"""
     unfold_QMEStates(el::QMEStates, symmetry) => el_unfold, isk_to_ik_isym
 Unfold QMEStates in a symmetry-reduced k grid to the full grid using `symmetry`.
 To ensure gauge consistency, all states in a given unfolded k point are unfolded from the
@@ -11,7 +70,7 @@ Output:
 - `isk_to_ik_isym`: Vector containing (ik, isym) that unfolds to k point isk.
 """
 function unfold_QMEStates(el::QMEStates, symmetry)
-    kpts_unfold = EPW.unfold_kpoints(el.kpts, symmetry)
+    kpts_unfold = unfold_kpoints(el.kpts, symmetry)
     isk_to_ik_isym = fill((-1, -1), kpts_unfold.n)
     ik_unfold = empty(el.ik)
     ib1_unfold = empty(el.ib1)
@@ -75,17 +134,17 @@ function unfold_scattering_out_matrix(S_out_irr, el_irr, el, ik_to_ikirr_isym)
     sp_i = Int[]
     sp_j = Int[]
     sp_val = eltype(S_out_irr)[]
-    indmap = EPW.states_index_map(el)
-    indmap_irr = EPW.states_index_map(el_irr)
+    indmap = states_index_map(el)
+    indmap_irr = states_index_map(el_irr)
     for i in 1:el.n
         (; ik, ib1, ib2) = el[i]
         ikirr, _ = ik_to_ikirr_isym[ik]
-        i_irr = get(indmap_irr, EPW.CI(ib1, ib2, ikirr), -1)
+        i_irr = get(indmap_irr, CI(ib1, ib2, ikirr), -1)
         i_irr == -1 && continue
         for ib3 in el.ib_rng[ik], ib4 in el.ib_rng[ik]
-            j = get(indmap, EPW.CI(ib3, ib4, ik), -1)
+            j = get(indmap, CI(ib3, ib4, ik), -1)
             j == -1 && continue
-            j_irr = get(indmap_irr, EPW.CI(ib3, ib4, ikirr), -1)
+            j_irr = get(indmap_irr, CI(ib3, ib4, ikirr), -1)
             j_irr == -1 && continue
             val = S_out_irr[i_irr, j_irr]
             if abs(val) > 0
@@ -106,14 +165,14 @@ TODO: Generalize ``symop.Scart * x[i]`` to work with any datatype (scalar, vecto
 """
 function unfold_QMEVector(f_irr::QMEVector{ElType, FT}, model::QMEIrreducibleKModel, trodd, invodd) where {ElType, FT}
     @assert f_irr.state === model.el_irr
-    indmap_irr = EPW.states_index_map(model.el_irr)
+    indmap_irr = states_index_map(model.el_irr)
     f = QMEVector(model.el, ElType)
     for i in 1:model.el.n
         (; ik, ib1, ib2) = model.el[i]
 
         ik_irr, isym = model.ik_to_ikirr_isym[ik]
         symop = model.symmetry[isym]
-        i_irr = indmap_irr[EPW.CI(ib1, ib2, ik_irr)]
+        i_irr = indmap_irr[CI(ib1, ib2, ik_irr)]
 
         f[i] = symop.Scart * f_irr[i_irr]
         if trodd && symop.is_tr
