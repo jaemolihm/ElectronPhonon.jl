d5884197e934527a349165bc881c17f57b59d499
Jae-Mo Lihm <jaemolim96@gmail.com>
2022-03-02 15:49:00 +0900

    Add test for QMEStates indmap

--- test/boltzmann/test_QMEVector.jl
+++ test/boltzmann/test_QMEVector.jl
@@ -1,9 +1,8 @@
 using Test
 using EPW
-using HDF5
 using LinearAlgebra
 
-@testset "QMEVector" begin
+@testset "QMEStates" begin
     BASE_FOLDER = dirname(dirname(pathof(EPW)))
     folder = joinpath(BASE_FOLDER, "test", "data_cubicBN")
     model = load_model(folder)
@@ -14,38 +13,61 @@ using LinearAlgebra
     el_k_save = compute_electron_states(model, kpts, ["eigenvalue", "velocity"]);
     el = EPW.electron_states_to_QMEStates(el_k_save, kpts, qme_offdiag_cutoff, 0.)
 
-    # Test basic operations
-    x = QMEVector(el, rand(el.n))
-    y = QMEVector(el, rand(ComplexF64, el.n))
-    @test eltype(x) === eltype(x.data)
-    @test eltype(y) === eltype(y.data)
-    @test size(x) == el.n
-    @test x[3] == x.data[3]
-    # @test x[10:20] == x.data[10:20] # This does not currently work
+    @testset "indmap" begin
+        # Test indmap
+        cnt = 0
+        for ik in 1:el.kpts.n
+            for ib2 in (minimum(el.ib2)-1):(maximum(el.ib2)+1)
+                for ib1 in (minimum(el.ib1)-1):(maximum(el.ib1)+1)
+                    i = EPW.get_1d_index(el, ib1, ib2, ik)
+                    if i == 0
+                        @assert EPW.hasstate(el, ib1, ib2, ik) == false
+                    elseif 1 <= i <= el.n
+                        @assert EPW.hasstate(el, ib1, ib2, ik) == true
+                        @assert (ik, ib1, ib2) == (el.ik[i], el.ib1[i], el.ib2[i])
+                        cnt += 1
+                    else
+                        error("wrong i")
+                    end
+                end
+            end
+        end
+        @assert cnt == el.n
+    end
 
-    # Test basic arithmetic operations
-    z = 2 * x - y / 3 + x * 0.5
-    @test z.state === el
-    @test z.data ≈ @. 2 * x.data - y.data / 3 + 0.5 * x.data
-    @test length(z.data) == el.n
-    A = rand(el.n, el.n)
-    @test (A * y).state === el
-    @test (A * y).data ≈ A * y.data
-    @test (A \ y).data ≈ inv(A) * y.data
+    @testset "QMEVector" begin
+        # Test basic operations
+        x = QMEVector(el, rand(el.n))
+        y = QMEVector(el, rand(ComplexF64, el.n))
+        @test eltype(x) === eltype(x.data)
+        @test eltype(y) === eltype(y.data)
+        @test size(x) == el.n
+        @test x[3] == x.data[3]
+        # @test x[10:20] == x.data[10:20] # This does not currently work
 
-    # Test x * y as matrix multiplication
-    @test data_ik(x * y, 1) ≈ data_ik(x, 1) * data_ik(y, 1)
+        # Test basic arithmetic operations
+        z = 2 * x - y / 3 + x * 0.5
+        @test z.state === el
+        @test z.data ≈ @. 2 * x.data - y.data / 3 + 0.5 * x.data
+        @test length(z.data) == el.n
+        A = rand(el.n, el.n)
+        @test (A * y).state === el
+        @test (A * y).data ≈ A * y.data
+        @test (A \ y).data ≈ inv(A) * y.data
 
-    # Test get_velocity_as_QMEVector
-    v = EPW.get_velocity_as_QMEVector(el)
-    @test v isa Vec3
-    @test eltype(v) <: QMEVector
-    for i in 1:3
-        @test v[i].state === el
-        @test v[i].data ≈ [v[i] for v in el.v]
+        # Test x * y as matrix multiplication
+        @test data_ik(x * y, 1) ≈ data_ik(x, 1) * data_ik(y, 1)
+
+        # Test get_velocity_as_QMEVector
+        v = EPW.get_velocity_as_QMEVector(el)
+        @test v isa Vec3
+        @test eltype(v) <: QMEVector
+        for i in 1:3
+            @test v[i].state === el
+            @test v[i].data ≈ [v[i] for v in el.v]
+        end
     end
 end
-
 
 @testset "QMEVector symmetry" begin
     BASE_FOLDER = dirname(dirname(pathof(EPW)))
