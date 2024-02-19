113b4c706f2f09620bfa969f886ab577e9fe5dc8
Jae-Mo Lihm <jaemolim96@gmail.com>
2024-02-19 18:32:57 +0900

    Remove old custom LAPACK wrapper

--- src/common/AllocatedLAPACK.jl
+++ src/common/AllocatedLAPACK.jl
@@ -9,7 +9,6 @@ TODO: Add utility function to check runtime of different diagonalization routine
 
 module AllocatedLAPACK
 
-using Base.Threads
 using LinearAlgebra
 using LinearAlgebra: BlasInt, checksquare
 using LinearAlgebra.LAPACK: @blasfunc, chkstride1, liblapack, chklapackerror
@@ -121,93 +120,5 @@ If `uplo = U`, the upper triangle of `A` is used. If `uplo = L`, the lower trian
 The eigenvalues are returned as `ws.W` and the eigenvectors in `A`.
 """
 syev!(ws::HermitianEigenWsSYEV, jobz::AbstractChar, uplo::AbstractChar, A::AbstractMatrix; resize=true)
-
-
-
-
-# =========================================================================================
-# Old code
-
-# const _buffer_zheev = [HermitianEigenWsSYEV{ComplexF64,Float64}()]
-# const _buffer_cheev = [HermitianEigenWsSYEV{ComplexF32,Float32}()]
-
-# function __init__()
-#     Threads.resize_nthreads!(_buffer_zheev)
-#     Threads.resize_nthreads!(_buffer_cheev)
-# end
-
-# # Hermitian eigensolvers
-# for (syev, elty, relty, workspaces) in
-#     ((:zheev_,:ComplexF64,:Float64,:_buffer_zheev),
-#      (:cheev_,:ComplexF32,:Float32,:_buffer_cheev))
-#     @eval begin
-#         # SUBROUTINE ZHEEV( JOBZ, UPLO, N, A, LDA, W, WORK, LWORK, RWORK, INFO )
-#         # *     .. Scalar Arguments ..
-#         #       CHARACTER          JOBZ, UPLO
-#         #       INTEGER            INFO, LDA, LWORK, N
-#         # *     ..
-#         # *     .. Array Arguments ..
-#         #       DOUBLE PRECISION   RWORK( * ), W( * )
-#         #       COMPLEX*16         A( LDA, * ), WORK( * )
-#         function epw_syev!(jobz::AbstractChar, uplo::AbstractChar, A::AbstractMatrix{$elty})
-#             n = checksquare(A)
-#             workspace = $workspaces[threadid()]
-#             if n > workspace.n
-#                 # Need to regenerate workspace
-#                 workspace = _epw_syev_alloc(jobz, uplo, A)
-#                 $workspaces[threadid()] = workspace
-#             end
-#             _epw_syev!(jobz, uplo, A, workspace.W, workspace)
-#         end
-
-#         function epw_syev!(jobz::AbstractChar, uplo::AbstractChar, A::AbstractMatrix{$elty}, W::AbstractVector{$relty})
-#             n = checksquare(A)
-#             ws = $workspaces[threadid()]
-#             if n > ws.n
-#                 # Need to regenerate workspace
-#                 ws = _epw_syev_alloc(jobz, uplo, A)
-#                 $workspaces[threadid()] = ws
-#             end
-#             _epw_syev!(jobz, uplo, A, W, ws)
-#         end
-
-#         function _epw_syev!(jobz::AbstractChar, uplo::AbstractChar, A::AbstractMatrix{$elty},
-#                 W::AbstractVector{$relty}, ws::HermitianEigenWsSYEV{$elty,$relty})
-#             chkstride1(A)
-#             n = checksquare(A)
-#             work  = ws.work
-#             rwork = ws.rwork
-#             info  = Ref{BlasInt}()
-#             ccall((@blasfunc($syev), liblapack), Cvoid,
-#                     (Ref{UInt8}, Ref{UInt8}, Ref{BlasInt}, Ptr{$elty}, Ref{BlasInt},
-#                     Ptr{$relty}, Ptr{$elty}, Ref{BlasInt}, Ptr{$relty}, Ptr{BlasInt},
-#                     Clong, Clong),
-#                     jobz, uplo, n, A, stride(A,2),
-#                     W, work, length(ws.work), rwork, info,
-#                     1, 1)
-#             chklapackerror(info[])
-#             W, A
-#         end
-
-#         function _epw_syev_alloc(jobz::AbstractChar, uplo::AbstractChar, A::AbstractMatrix{$elty})
-#             chkstride1(A)
-#             n = checksquare(A)
-#             W     = similar(A, $relty, n)
-#             work  = Vector{$elty}(undef, 1)
-#             lwork = BlasInt(-1)
-#             rwork = Vector{$relty}(undef, max(1, 3n-2))
-#             info  = Ref{BlasInt}()
-#             ccall((@blasfunc($syev), liblapack), Cvoid,
-#                     (Ref{UInt8}, Ref{UInt8}, Ref{BlasInt}, Ptr{$elty}, Ref{BlasInt},
-#                     Ptr{$relty}, Ptr{$elty}, Ref{BlasInt}, Ptr{$relty}, Ptr{BlasInt},
-#                     Clong, Clong),
-#                     jobz, uplo, n, A, stride(A,2), W, work, lwork, rwork, info,
-#                     1, 1)
-#             chklapackerror(info[])
-#             resize!(work, BlasInt(real(work[1])))
-#             HermitianEigenWsSYEV{$elty,$relty}(W, work, rwork)
-#         end
-#     end
-# end
 
 end
