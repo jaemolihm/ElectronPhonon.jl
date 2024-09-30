a9ef6a3332d855c9cae542e645cac53f8339ee2e
Jae-Mo Lihm <jaemolim96@gmail.com>
2024-09-30 13:20:37 +0200

    Fix effective mass

--- src/postprocess/band_structures.jl
+++ src/postprocess/band_structures.jl
@@ -4,7 +4,7 @@ export find_band_extrema
 export find_effective_mass
 
 """
-    find_band_extrema(model, iband; type, nk=50)
+    find_band_extrema(model, iband, type; nk=50)
 Find the extremum of the `iband`-th band.
 * `type`: `:Minima` to find band minima, `:Maxima` to find band maxima.
 """
@@ -39,20 +39,22 @@ function find_band_extrema(model, iband, type; nk=50)
 
     xk_equiv .= normalize_kpoint_coordinate(xk_equiv)
 
+    @info "$type of the $iband-th band = $(e / unit_to_aru(:eV)) eV at $xk_equiv (crystal)."
+
     Kpoints(xk_equiv), e
 end
 
 
 """
     find_effective_mass(model, kpts, iband)
-Compute the effective mass for each k point in `kpts` for the `iband`-th band.
+Compute the effective mass in Cartesian coordinates for each k point in `kpts` for the `iband`-th band.
 
 In the Rydberg units, ``e(k + δk) = e0 + δk * m⁻¹ * δk``. So, ``m = 2(d²e / dδk²)⁻¹``.
 """
 function find_effective_mass(model, kpts, iband)
-    f(xk) = compute_eigenvalues_el(model, Kpoints(xk); fourier_mode="normal")[iband]
-    map(kpts.vectors) do xk
-        hessian = jacobian(central_fdm(5, 1), xk -> grad(central_fdm(5, 1, adapt=0), f, xk)[1], xk)[1]
+    f(xk) = compute_eigenvalues_el(model, Kpoints(model.recip_lattice \ xk); fourier_mode="normal")[iband]
+    map(kpts.vectors) do xk0
+        hessian = jacobian(central_fdm(5, 1), xk -> grad(central_fdm(5, 1, adapt=0), f, xk)[1], model.recip_lattice * xk0)[1]
         2 * inv(Mat3(hessian))
     end
 end
